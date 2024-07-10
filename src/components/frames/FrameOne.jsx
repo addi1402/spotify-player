cd6ade6f19fb06e8eb4e867f9f1f72193568470d
Aditya Patil <addi1402@gmail.com>
2024-07-10 12:12:59 +0530

    Fix Avatar Fallback

--- src/components/frames/FrameOne.jsx
+++ src/components/frames/FrameOne.jsx
@@ -21,9 +21,15 @@ const FrameOne = () => {
         />
       </a>
 
-      <Avatar className="w-8 h-auto lg:w-10 cursor-pointer">
-        <AvatarImage src="https://github.com/shadcn.png" />
-        <AvatarFallback>Profile</AvatarFallback>
+      <Avatar
+        alt="User Avatar"
+        className="w-8 h-8 lg:w-10 lg:h-10 cursor-pointer text-white"
+      >
+        <AvatarImage
+          src="/profile.png"
+          className="object-cover bg-neutral-700"
+        />
+        <AvatarFallback className="bg-neutral-700">CN</AvatarFallback>
       </Avatar>
     </div>
   );
